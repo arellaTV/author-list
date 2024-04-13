67f5963ada09226d96d3c50dfaaf3a592054ec53
arellaTV <jeremiah.arella@gmail.com>
2024-04-13 00:08:51 -0700

    Renders authors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+async function getAuthors() {
+  const res = await fetch("http://localhost:8080/authors");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const authors = await getAuthors();
+
   return (
     <main className={styles.main}>
       <div className={styles["card-container"]}>
@@ -15,25 +30,37 @@ export default function Home() {
           />
           <h1 className={styles.header}>You have 10 Team Members</h1>
           <div>
-            <div className={styles["team-member"]}>
-              <Image
-                src="/images/profile_photo.png"
-                alt="Profile photo of Jenny Appleseed"
-                width={48}
-                height={48}
-                className={styles["profile-photo"]}
-              />
-              <div className={styles["team-member__details"]}>
-                <div>Jenny Appleseed</div>
-                <div>jenny.appleseed@example.com</div>
-              </div>
-              <Image
-                src="/icons/x_circle.svg"
-                alt="Dismiss"
-                width={32}
-                height={32}
-              />
-            </div>
+            {authors.map((author) => {
+              return (
+                <div
+                  className={styles["team-member"]}
+                  key={`author-${author?.id}`}
+                >
+                  <Image
+                    src="/images/profile_photo.png"
+                    alt={`Profile photo of ${author?.author_name}`}
+                    width={48}
+                    height={48}
+                    className={styles["profile-photo"]}
+                  />
+                  <div className={styles["team-member__details"]}>
+                    <div className={styles["team-member__name"]}>
+                      {author?.author_name}
+                    </div>
+                    <div className={styles["team-member__email"]}>
+                      {author?.author_email}
+                    </div>
+                  </div>
+                  <Image
+                    src="/icons/x_circle.svg"
+                    alt="Dismiss"
+                    width={32}
+                    height={32}
+                    className={styles["team-member__dismiss"]}
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
