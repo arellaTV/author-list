73adec8a513936b56ca4f608c844e2184080627d
arellaTV <jeremiah.arella@gmail.com>
2024-04-12 17:25:10 -0700

    Installs sequelize as dep and connects to PG db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello there!");
+const { Sequelize } = require("sequelize");
+
+const DB_CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING ||
+  "postgres://krikey_user:r@nd0mv@lu3@localhost:5432/krikey_db";
+
+const sequelize = new Sequelize(DB_CONNECTION_STRING);
+
+(async function () {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
