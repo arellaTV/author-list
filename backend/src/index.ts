9e208c9d4cd20af8eb08b5033d1d165ee586ce57
arellaTV <jeremiah.arella@gmail.com>
2024-04-12 18:19:43 -0700

    Adds /authors endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,71 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import {
+  ContainerTypes,
+  ExpressJoiError,
+  ValidatedRequest,
+  ValidatedRequestSchema,
+  createValidator,
+} from "express-joi-validation";
+import * as Joi from "joi";
 import { Author } from "./models/Author";
 import db from "./config/database";
 import { Book } from "./models/Book";
 import { SaleItem } from "./models/SaleItem";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+const validator = createValidator({ passError: true });
+
+const querySchema = Joi.object({
+  name: Joi.string().optional(),
+});
+
+interface AuthorRequestSchema extends ValidatedRequestSchema {
+  [ContainerTypes.Query]: {
+    name: string;
+  };
+}
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.get(
+  "/authors",
+  validator.query(querySchema),
+  (req: ValidatedRequest<AuthorRequestSchema>, res: Response) => {
+    const { name } = req.query;
+
+    const nameSanitized = name
+      ?.toLowerCase()
+      ?.replaceAll(" ", "")
+      .replace(/\W/g, "");
+    res.json({
+      message: nameSanitized || "OK",
+    });
+  }
+);
+
+app.use(
+  (
+    err: any | ExpressJoiError,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (err && err.error && err.error.isJoi) {
+      res.status(400).json({
+        type: err.type,
+        message: err.error.toString(),
+      });
+    } else {
+      next(err);
+    }
+  }
+);
 
 const initApp = async () => {
   try {
@@ -16,6 +80,10 @@ const initApp = async () => {
 
     const saleItems = await SaleItem.findAll();
     console.log("All saleItems:", JSON.stringify(saleItems, null, 2));
+
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
