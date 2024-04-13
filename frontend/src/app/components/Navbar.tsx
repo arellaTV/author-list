2a16ccb5c1a7cefff76cc67bef486e2a7785c88e
arellaTV <jeremiah.arella@gmail.com>
2024-04-13 00:43:49 -0700

    Adds chevron to nav links

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -20,21 +20,56 @@ const Navbar = (props: Props) => {
         <nav className={styles.navbar__links}>
           <Link href="/" className={styles.navbar__link}>
             How to Animate
+            <Image
+              className={styles.chevron}
+              src="/icons/chevron_down.svg"
+              width={24}
+              height={23}
+              alt="Expand How to Animate"
+            />
           </Link>
           <Link href="/" className={styles.navbar__link}>
             Business
+            <Image
+              className={styles.chevron}
+              src="/icons/chevron_down.svg"
+              width={24}
+              height={23}
+              alt="Expand Business"
+            />
           </Link>
           <Link href="/" className={styles.navbar__link}>
             Education
+            <Image
+              className={styles.chevron}
+              src="/icons/chevron_down.svg"
+              width={24}
+              height={23}
+              alt="Expand Education"
+            />
           </Link>
           <Link href="/" className={styles.navbar__link}>
             Social Media
+            <Image
+              className={styles.chevron}
+              src="/icons/chevron_down.svg"
+              width={24}
+              height={23}
+              alt="Expand Social Media"
+            />
           </Link>
           <Link href="/" className={styles.navbar__link}>
             Pricing
           </Link>
           <Link href="/" className={styles.navbar__link}>
             About Us
+            <Image
+              className={styles.chevron}
+              src="/icons/chevron_down.svg"
+              width={24}
+              height={23}
+              alt="Expand How to Animate"
+            />
           </Link>
         </nav>
         <Link href="/" className={styles["get-started"]}>
